e98f104180e220c268191460d423dbe90fabc6af
Dmitrii Lytkin <lytkin1@yandex.ru>
2018-03-10 13:29:32 +0400

    Initial Commit

---
+++ holiday-events-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  holiday-events-ios
+//
+//  Created by Дмитрий Лыткин on 10.03.2018.
+//  Copyright © 2018 Дмитрий Лыткин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
